ddec01240f3d46d985dc5d0eb3e022e90b7ad514
vm3do <medoavvadi@gmail.com>
2025-10-03 01:20:32 +0100

    feat: add database connection with Singleton pattern

---
+++ src/config/DBConnection.java
@@ -0,0 +1,38 @@
+package config;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static DBConnection instance;
+        private Connection connection;
+
+
+        private DBConnection() {
+            try{
+                String URL = "jdbc:mysql://127.0.0.1:3306/fraude_detection";
+                String USERNAME = "root";
+                String PASSWORD = "root";
+
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        public static DBConnection getInstance() {
+            if (instance == null) {
+                instance = new DBConnection();
+            }
+            return instance;
+        }
+
+        public Connection getConnection() {
+            return connection;
+        }
+
+}
